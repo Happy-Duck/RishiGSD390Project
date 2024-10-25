61cd3750948f1483dae603735176b57e71a2d467
Happy-Duck <66819856+Happy-Duck@users.noreply.github.com>
2024-10-25 01:04:33 -0500

    Initial commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //It's dangerous to go alone, take this!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
